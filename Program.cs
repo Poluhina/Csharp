b08beba8e588fd4e9b38076825994b0196725e65
Poluhina <nastyapoluhina04@gmail.com>
2025-09-25 12:35:25 +0300

    Первый коммит: добавлен проект

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+    Console.WriteLine("Hello, Jerry");
+
+    void PrintABC()
+     {
+         Console.WriteLine("A");
+         Console.WriteLine("B");
+         Console.WriteLine("C");
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        PrintABC();
+        Thread.Sleep(500); // задержка 0.5 сек
+    }
+
+    
+    void B()
+    {
+        Console.WriteLine("Это функция B");
+    }
+
+    void C()
+    {
+        Console.WriteLine("Это функция C");
+    }
+
+    void A()
+    {
+        B();
+        C();
+    }
+
+    A();
+    A();
+
+
